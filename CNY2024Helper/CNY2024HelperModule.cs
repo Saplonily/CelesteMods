540caf822851bd5045e1bf52dda34d85c5bae108
Saplonily <Saplonily@outlook.com>
2024-08-08 23:12:07 +0800

    Revert "Remove VivCassetteTileEntityFix"

--- CNY2024Helper/CNY2024HelperModule.cs
+++ CNY2024Helper/CNY2024HelperModule.cs
@@ -3,13 +3,21 @@ namespace Celeste.Mod.CNY2024Helper;
 
 public class CNY2024HelperModule : EverestModule
 {
+    public static CNY2024HelperModule Instance { get; private set; }
+
+    public override Type SettingsType => typeof(CNY2024HelperSettings);
+    public static CNY2024HelperSettings Settings => (CNY2024HelperSettings)Instance._Settings;
+
     public override void Load()
     {
+        Instance = this;
         DrivableCarLightModule.Load();
+        VivCassetteTileEntityFix.Load();
     }
 
     public override void Unload()
     {
         DrivableCarLightModule.Unload();
+        VivCassetteTileEntityFix.Unload();
     }
 }
